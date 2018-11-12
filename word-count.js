a73e3fba6e11a015c01d80b3c739581fdbe6dbe8
Rebecca Turner <me@re-becca.org>
2018-11-12 15:47:05 -0800

    fix: es5 our module, because why not?

--- word-count.js
+++ word-count.js
@@ -3,7 +3,7 @@ const wordChars = {{word-chars}}
 
 module.exports = function (str) {
   if (typeof str !== 'string') str = str == null ? '' : String(str)
-  let cnt = 0
+  var cnt = 0
   while (wordChars.test(str)) ++cnt
   return cnt
 }
